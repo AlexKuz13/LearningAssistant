0b83e370b9a0923260544f22d0aa9849955676b1
AlexKuz13 <kuznetsov356@gmail.com>
2021-02-06 14:39:41 +0300

    Begin Register

--- app/src/main/java/com/example/learningassistant/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/learningassistant/ui/fragments/SettingsFragment.kt
@@ -1,17 +1,28 @@
 package com.example.learningassistant.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.learningassistant.R
 import com.example.learningassistant.utilits.APP_ACTIVITY
+import com.example.learningassistant.utilits.showToast
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title="Настройки"
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.settings_action_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.settings_menu_change_name -> showToast("Изменить имя")
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/example/learningassistant/ui/fragments/register/RegisterFragment.kt
@@ -0,0 +1,13 @@
+package com.example.learningassistant.ui.fragments.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.learningassistant.R
+
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+}
