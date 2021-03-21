c9f0b7b81a219da244fad074121407f560c989c5
AlexKuz13 <kuznetsov356@gmail.com>
2021-03-21 22:08:31 +0300

    Addition for Physic telephones

--- app/src/main/java/com/example/learningassistant/ui/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/example/learningassistant/ui/fragments/register/RegisterFragment.kt
@@ -2,7 +2,7 @@ package com.example.learningassistant.ui.fragments.register
 
 import androidx.fragment.app.Fragment
 import com.example.learningassistant.R
-import com.example.learningassistant.database.AUTH
+import com.example.learningassistant.database.*
 import com.example.learningassistant.utilits.*
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -16,50 +16,73 @@ import java.util.concurrent.TimeUnit
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
 
-    private lateinit var mCallBack:PhoneAuthProvider.OnVerificationStateChangedCallbacks
-    private lateinit var mPhoneNumber:String
+    private lateinit var mCallBack: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var mPhoneNumber: String
 
     override fun onStart() {
         super.onStart()
-        mCallBack=object:PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+        mCallBack = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
                 AUTH.signInWithCredential(credential)
                     .addOnSuccessListener {
-                        showToast("C возвращением!")
-                        restartActivity()
-                        }
-                    .addOnFailureListener { showToast(it.message.toString()) }
+                        val uid = AUTH.currentUser?.uid.toString()
+                        DB.collection(COLL_USERS).document(uid).get()
+                            .addOnCompleteListener {
+                                if (it.result?.exists() != true) {
+                                    USER.id = uid
+                                    USER.phone = mPhoneNumber
+                                    DB.collection(COLL_USERS).document(uid).set(USER)
+                                        .addOnSuccessListener {
+                                            val map = hashMapOf("rating_sum" to 0.0)
+                                            DB.collection(COLL_RATINGS).document(uid)
+                                                .set(map)
+                                                .addOnFailureListener {
+                                                    showToast(it.message.toString())
+                                                }
+                                            showToast("Добро пожаловать")
+                                            APP_ACTIVITY.hideKeyboard()
+                                            restartActivity()
+                                        }
+                                } else {
+                                    showToast("C возвращением!")
+                                    restartActivity()
+                                }
+                            }
+                            .addOnFailureListener { showToast(it.message.toString()) }
+                    }
             }
 
-            override fun onVerificationFailed(p0: FirebaseException) {
-                showToast(p0.message.toString())
-            }
-
-            override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
-                replaceFragment(EnterCodeFragment(mPhoneNumber,id))
-            }
+        override fun onVerificationFailed(p0: FirebaseException) {
+            showToast(p0.message.toString())
         }
-        register_btn_next.setOnClickListener {
-            sentCode()
+
+        override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
+            replaceFragment(EnterCodeFragment(mPhoneNumber, id))
         }
     }
 
-    private fun sentCode() {
-        if (register_input_phone_number.text.toString().isEmpty()) {
-            showToast("Введите номер телефона")
-        } else
-           authUser()
+    register_btn_next.setOnClickListener{
+        sentCode()
     }
+}
 
-    private fun authUser() {
-        mPhoneNumber=register_input_phone_number.text.toString()
-        PhoneAuthProvider.verifyPhoneNumber(
-            PhoneAuthOptions
-                .newBuilder(FirebaseAuth.getInstance())
-                .setActivity(APP_ACTIVITY)
-                .setPhoneNumber(mPhoneNumber)
-                .setTimeout(60L, TimeUnit.SECONDS)
-                .setCallbacks(mCallBack)
-                .build())
-    }
+private fun sentCode() {
+    if (register_input_phone_number.text.toString().isEmpty()) {
+        showToast("Введите номер телефона")
+    } else
+        authUser()
+}
+
+private fun authUser() {
+    mPhoneNumber = register_input_phone_number.text.toString()
+    PhoneAuthProvider.verifyPhoneNumber(
+        PhoneAuthOptions
+            .newBuilder(FirebaseAuth.getInstance())
+            .setActivity(APP_ACTIVITY)
+            .setPhoneNumber(mPhoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setCallbacks(mCallBack)
+            .build()
+    )
+}
 }
